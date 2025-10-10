6d088eed2e1a94055fbd7f097ffb227ab01f2320
parallakss <114648355+parallakss@users.noreply.github.com>
2025-10-10 10:00:00 +0000

    lab4

--- lab4/task2.asm
+++ lab4/task2.asm
@@ -15,22 +15,20 @@ _start:
     mov rdx, prompt_len
     syscall
 
-    ; Читаем ввод пользователя
     mov rax, SYS_READ
     mov rdi, 0
     mov rsi, input_buffer
     mov rdx, 32
     syscall
 
-    ; Преобразуем строку в число
     mov rdi, input_buffer
     xor rax, rax
     xor rcx, rcx
 convert_loop:
     mov cl, [rdi]
-    cmp cl, 10             ; остановиться на переводе строки
+    cmp cl, 10            
     je done_convert
-    cmp cl, 0              ; или на нулевом байте
+    cmp cl, 0              
     je done_convert
     sub cl, '0'
     imul rax, 10
@@ -39,73 +37,57 @@ convert_loop:
     jmp convert_loop
 done_convert:
 
-    ; Сохраняем n
     mov [n], rax
 
-    ; Вычисляем сумму ряда
     call compute_sum
 
-    ; Выводим результат
     call print_result
 
-    ; Завершаем программу
     mov rax, SYS_EXIT
     xor rdi, rdi
     syscall
 
 compute_sum:
-    ; Инициализация суммы S = 0
+
     xor r12, r12
     
-    ; k начинается с 1 до n
     mov r8, 1
-    mov r9, 1              ; начальный знак = +1 (т.к. (-1)^(1+1) = 1)
+    mov r9, 1             
 
 sum_loop:
-    ; Вычисляем k²
     mov rax, r8
-    imul rax, rax          ; rax = k²
+    imul rax, rax          
 
-    ; Умножаем на знак (-1)^(k+1)
-    imul rax, r9           ; rax = (-1)^(k+1) * k²
+    imul rax, r9           
 
-    ; Добавляем к сумме
     add r12, rax
 
-    ; Меняем знак для следующей итерации
     neg r9
     
-    ; Увеличиваем k
     inc r8
     cmp r8, [n]
     jle sum_loop
     
-    ; Сохраняем результат
     mov [result], r12
     ret
 
 print_result:
-    ; Выводим "Result: "
     mov rax, SYS_WRITE
     mov rdi, 1
     mov rsi, result_label
     mov rdx, result_label_len
     syscall
 
-    ; Выводим число
     mov rax, [result]
     mov rdi, number_buffer
     call int_to_string
     
-    ; Добавляем перевод строки
     mov byte [rdi], 10
     inc rdi
     
-    ; Вычисляем длину
     mov rdx, rdi
     sub rdx, number_buffer
-    
-    ; Выводим число
+
     mov rax, SYS_WRITE
     mov rdi, 1
     mov rsi, number_buffer
@@ -114,21 +96,18 @@ print_result:
     ret
 
 int_to_string:
-    ; Преобразует целое в строку
     test rax, rax
     jnz .convert
     
-    ; Случай числа 0
     mov byte [rdi], '0'
     inc rdi
     ret
     
 .convert:
-    ; Проверяем знак
+    
     cmp rax, 0
     jge .positive
     
-    ; Отрицательное число
     mov byte [rdi], '-'
     inc rdi
     neg rax
