9d8e1af4d317fa44fd80681a9bc507e94e78404c
parallakss <114648355+parallakss@users.noreply.github.com>
2025-10-10 08:37:08 +0000

    lab4last

--- Lab_4/task1.asm
+++ Lab_4/task1.asm
@@ -1,72 +1,173 @@
 section .data
-    prompt     db "Введите n: ", 0
-    format_in  db "%d", 0
-    format_out db "Количество чисел от 1 до %d, делящихся на 37 и 13: %d", 10, 0
+    prompt     db "Enter n: "
+    prompt_len equ $ - prompt
+    result1    db "Numbers from 1 to "
+    result2    db " divisible by 37 and 13: "
+    newline    db 10
 
 section .bss
-    n resd 1
+    input_buf resb 12
+    n_str     resb 12
+    count_str resb 12
 
 section .text
-    global main
-    extern printf, scanf
+    global _start
 
-main:
-    push rbp
-    mov rbp, rsp
-    
+_start:
     ; Вывод приглашения
-    mov rdi, prompt
-    xor eax, eax
-    call printf
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt
+    mov rdx, prompt_len
+    syscall
 
-    ; Ввод n
-    mov rdi, format_in
-    mov rsi, n
-    xor eax, eax
-    call scanf
+    ; Ввод n через системный вызов read
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input_buf
+    mov rdx, 12
+    syscall
 
-    ; Инициализация счетчиков
-    mov ecx, 1          ; i = 1
-    xor ebx, ebx        ; count = 0
-    mov esi, [n]        ; ESI = n
+    ; Преобразование строки в число
+    mov rsi, input_buf
+    call atoi
+    mov r8, rax        ; сохраняем n в r8
 
-loop_start:
-    cmp ecx, esi
-    jg end_loop
+    ; Подсчет чисел
+    mov r9, 1          ; i = 1
+    xor r10, r10       ; count = 0
+
+count_loop:
+    cmp r9, r8
+    jg end_count
 
     ; Проверка деления на 37
-    mov eax, ecx
-    xor edx, edx
-    mov edi, 37
-    div edi
-    test edx, edx
-    jnz next_number
+    mov rax, r9
+    xor rdx, rdx
+    mov rbx, 37
+    div rbx
+    test rdx, rdx
+    jnz next_num
 
     ; Проверка деления на 13
-    mov eax, ecx
-    xor edx, edx
-    mov edi, 13
-    div edi
-    test edx, edx
-    jnz next_number
+    mov rax, r9
+    xor rdx, rdx
+    mov rbx, 13
+    div rbx
+    test rdx, rdx
+    jnz next_num
 
     ; Оба условия выполнены
-    inc ebx
+    inc r10
 
-next_number:
-    inc ecx
-    jmp loop_start
+next_num:
+    inc r9
+    jmp count_loop
 
-end_loop:
-    ; Вывод результата
-    mov rdi, format_out
-    mov esi, [n]
-    mov edx, ebx
-    xor eax, eax
-    call printf
+end_count:
+    ; Вывод первой части результата
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, result1
+    mov rdx, 17
+    syscall
+
+    ; Преобразование n в строку и вывод
+    mov rax, r8
+    mov rdi, n_str
+    call itoa
+    mov rsi, n_str
+    call print_str
+
+    ; Вывод второй части результата
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, result2
+    mov rdx, 24
+    syscall
+
+    ; Преобразование count в строку и вывод
+    mov rax, r10
+    mov rdi, count_str
+    call itoa
+    mov rsi, count_str
+    call print_str
+
+    ; Новая строка
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, newline
+    mov rdx, 1
+    syscall
 
     ; Завершение программы
-    pop rbp
     mov rax, 60
     xor rdi, rdi
     syscall
+
+; Преобразование строки в число (atoi)
+atoi:
+    xor rax, rax
+    xor rcx, rcx
+atoi_loop:
+    mov cl, [rsi]
+    cmp cl, 10
+    je atoi_done
+    cmp cl, '0'
+    jb atoi_done
+    cmp cl, '9'
+    ja atoi_done
+    sub cl, '0'
+    imul rax, 10
+    add rax, rcx
+    inc rsi
+    jmp atoi_loop
+atoi_done:
+    ret
+
+; Преобразование числа в строку (itoa)
+itoa:
+    mov rbx, 10
+    mov rcx, 0
+    test rax, rax
+    jnz itoa_loop
+    mov byte [rdi], '0'
+    mov byte [rdi+1], 0
+    ret
+itoa_loop:
+    xor rdx, rdx
+    div rbx
+    add dl, '0'
+    push rdx
+    inc rcx
+    test rax, rax
+    jnz itoa_loop
+    mov rsi, rdi
+itoa_pop:
+    pop rax
+    mov [rdi], al
+    inc rdi
+    loop itoa_pop
+    mov byte [rdi], 0
+    ret
+
+; Вывод строки
+print_str:
+    mov rdi, rsi
+    call strlen
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+; Длина строки
+strlen:
+    xor rax, rax
+strlen_loop:
+    cmp byte [rdi+rax], 0
+    je strlen_done
+    inc rax
+    jmp strlen_loop
+strlen_done:
+    ret
--- Lab_4/task10.asm
+++ Lab_4/task10.asm
@@ -1,85 +1,178 @@
 section .data
     correct_pass db "secret123", 0
-    prompt       db "Введите пароль (осталось попыток: ", 0
-    attempt_msg  db "): ", 0
-    wrong_pass   db "Неверный пароль", 10, 0
-    success      db "Вход", 10, 0
-    failure      db "Неудача", 10, 0
-    format_int   db "%d", 0
-    format_str   db "%19s", 0    ; ограничение длины ввода
+    prompt       db "Enter password (attempts left: "
+    prompt_len   equ $ - prompt
+    attempt_msg  db "): "
+    attempt_len  equ $ - attempt_msg
+    wrong_pass   db "Wrong password", 10
+    wrong_len    equ $ - wrong_pass
+    success      db "Access granted", 10
+    success_len  equ $ - success
+    failure      db "Access denied", 10
+    failure_len  equ $ - failure
 
 section .bss
-    password  resb 20
-    attempts  resd 1
+    input_buf resb 20
+    attempts_str resb 4
 
 section .text
-    global main
-    extern printf, scanf, strcmp
+    global _start
 
-main:
-    push rbp
-    mov rbp, rsp
-    
-    ; Инициализация счетчика попыток
-    mov dword [attempts], 5
+_start:
+    mov r12, 5          ; количество попыток
 
 auth_loop:
     ; Проверка количества попыток
-    cmp dword [attempts], 0
+    cmp r12, 0
     jle auth_failure
 
     ; Вывод приглашения
-    mov rdi, prompt
-    xor eax, eax
-    call printf
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, prompt
+    mov rdx, prompt_len
+    syscall
 
-    ; Вывод номера попытки
-    mov rdi, format_int
-    mov esi, [attempts]
-    xor eax, eax
-    call printf
+    ; Преобразование номера попытки в строку
+    mov rax, r12
+    mov rdi, attempts_str
+    call itoa
+    mov rsi, attempts_str
+    call print_str
 
-    mov rdi, attempt_msg
-    xor eax, eax
-    call printf
+    ; Вывод ": "
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, attempt_msg
+    mov rdx, attempt_len
+    syscall
 
-    ; Ввод пароля
-    mov rdi, format_str
-    mov rsi, password
-    xor eax, eax
-    call scanf
+    ; Ввод пароля через системный вызов read
+    mov rax, 0
+    mov rdi, 0
+    mov rsi, input_buf
+    mov rdx, 20
+    syscall
+
+    ; Удаление символа новой строки
+    mov rdi, input_buf
+    call remove_newline
 
     ; Сравнение паролей
-    mov rdi, password
-    mov rsi, correct_pass
+    mov rsi, input_buf
+    mov rdi, correct_pass
     call strcmp
-    test eax, eax
+    test rax, rax
     jz password_correct
 
     ; Пароль неверный
-    mov rdi, wrong_pass
-    xor eax, eax
-    call printf
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, wrong_pass
+    mov rdx, wrong_len
+    syscall
 
-    ; Уменьшение счетчика попыток
-    dec dword [attempts]
+    dec r12
     jmp auth_loop
 
 password_correct:
     ; Пароль верный
-    mov rdi, success
-    xor eax, eax
-    call printf
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, success
+    mov rdx, success_len
+    syscall
     jmp exit
 
 auth_failure:
-    mov rdi, failure
-    xor eax, eax
-    call printf
+    mov rax, 1
+    mov rdi, 1
+    mov rsi, failure
+    mov rdx, failure_len
+    syscall
 
 exit:
     ; Завершение программы
-    pop rbp
     mov rax, 60
     xor rdi, rdi
     syscall
+
+; Сравнение строк
+strcmp:
+strcmp_loop:
+    mov al, [rdi]
+    mov bl, [rsi]
+    cmp al, bl
+    jne strcmp_diff
+    test al, al
+    jz strcmp_equal
+    inc rdi
+    inc rsi
+    jmp strcmp_loop
+strcmp_equal:
+    xor rax, rax
+    ret
+strcmp_diff:
+    mov rax, 1
+    ret
+
+; Удаление символа новой строки
+remove_newline:
+    mov rsi, rdi
+remove_loop:
+    mov al, [rsi]
+    cmp al, 10
+    je found_newline
+    cmp al, 0
+    je remove_done
+    inc rsi
+    jmp remove_loop
+found_newline:
+    mov byte [rsi], 0
+remove_done:
+    ret
+
+; Функции itoa и print_str
+itoa:
+    mov rbx, 10
+    mov rcx, 0
+    test rax, rax
+    jnz itoa_loop
+    mov byte [rdi], '0'
+    mov byte [rdi+1], 0
+    ret
+itoa_loop:
+    xor rdx, rdx
+    div rbx
+    add dl, '0'
+    push rdx
+    inc rcx
+    test rax, rax
+    jnz itoa_loop
+    mov rsi, rdi
+itoa_pop:
+    pop rax
+    mov [rdi], al
+    inc rdi
+    loop itoa_pop
+    mov byte [rdi], 0
+    ret
+
+print_str:
+    mov rdi, rsi
+    call strlen
+    mov rdx, rax
+    mov rax, 1
+    mov rdi, 1
+    syscall
+    ret
+
+strlen:
+    xor rax, rax
+strlen_loop:
+    cmp byte [rdi+rax], 0
+    je strlen_done
+    inc rax
+    jmp strlen_loop
+strlen_done:
+    ret
